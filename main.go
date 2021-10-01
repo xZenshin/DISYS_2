17dba32bd0f404061fed587ac480f65aa9f1b54e
xZenshin <askoularsen@gmail.com>
2021-10-01 16:38:39 +0200

    add methods

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
-
 	// WARNING!
 	// Change this to a fully-qualified import path
 	// once you place this file into your project.
@@ -13,39 +13,34 @@ import (
 	//
 	//    sw "github.com/myname/myrepo/go"
 	//
-	sw "github.com/xZenshin/DISYS_2/go"
 )
+
+type Course struct {
+	Name string `json:"name,omitempty"`
+
+	CourseID int `json:"courseID,omitempty"`
+
+	Workload int32 `json:"workload,omitempty"`
+
+	StudentSatisfaction int32 `json:"studentSatisfaction,omitempty"`
+}
 
 func main() {
 	log.Printf("Server started")
 
 	router := gin.Default()
-	router.GET("/students", getStudents)
-	router.POST("/students", postStudent)
+	router.GET("/courses", getCourses)
+	router.POST("/course", postCourse)
+	router.GET("/course/:id", getCourse)
+	router.GET("/courseDel/:id", deleteCourse)
 
 	router.Run("localhost:8080")
 }
 
-var students = []sw.Student{}
-var courses = []sw.Course{}
-
-func postStudent(c *gin.Context) {
-	var newStudent sw.Student
-
-	if err := c.BindJSON(&newStudent); err != nil {
-		return
-	}
-
-	students = append(students, newStudent)
-	c.IndentedJSON(http.StatusCreated, newStudent)
-}
-
-func getStudents(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, students)
-}
+var courses = []Course{}
 
 func postCourse(c *gin.Context) {
-	var newCourse sw.Course
+	var newCourse Course
 
 	if err := c.BindJSON(&newCourse); err != nil {
 		return
@@ -57,4 +52,29 @@ func postCourse(c *gin.Context) {
 
 func getCourses(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, courses)
+}
+
+func getCourse(c *gin.Context) {
+	var id, _ = strconv.Atoi(c.Param("id"))
+	for _, a := range courses {
+		if a.CourseID == id {
+			c.IndentedJSON(http.StatusOK, a)
+		}
+	}
+}
+
+func deleteCourse(c *gin.Context) {
+	var index = 0
+	var id, _ = strconv.Atoi(c.Param("id"))
+	for _, a := range courses {
+
+		if a.CourseID == id {
+			courses = RemoveIndex(courses, index)
+			c.IndentedJSON(http.StatusOK, a)
+		}
+		index++
+	}
+}
+func RemoveIndex(s []Course, index int) []Course {
+	return append(s[:index], s[index+1:]...)
 }
